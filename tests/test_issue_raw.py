14ae0d315f6611f83d302ce657c0182e5e6cbbe0
Andrei Monichev <vyroud@gmail.com>
2024-12-14 18:39:09 +0300

    git added test 1

---
+++ tests/test_issue_raw.py
@@ -0,0 +1,12 @@
+from selene import by, be, browser
+from selene.support.shared.jquery_style import s
+
+
+def test_issue_name():
+    #browser.open("https://github.com")
+    s("[data-target='qbsearch-input.inputButtonText']").click()
+    s("#query-builder-test").click().type("Vyroum/qa_hmw10").press_enter()
+    s(by.link_text("Vyroum/qa_hmw10")).click()
+
+    s("#issues-tab").click()
+    s(by.partial_text("homework")).should(be.visible)
